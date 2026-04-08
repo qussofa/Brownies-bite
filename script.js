5c23baf29b53d831051ccdafd2326be57de23edc
AZAR <zar@gmail.com>
2026-04-08 22:26:45 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+function scrollToSection() {
+    document.getElementById("services").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+function scrollProduk() {
+    document.getElementById("produk").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+
+function filterProduk(kategori) {
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        if (kategori === "all") {
+            card.style.display = "block";
+        } else {
+            if (card.dataset.kategori === kategori) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+        }
+    });
+}
+
+function page(num) {
+    alert("Pindah ke halaman " + num);
+}
+
+
+function scrollProduk() {
+    document.getElementById("produk").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+function gantiToko(id) {
+    const title = document.getElementById("storeTitle");
+    const alamat = document.getElementById("storeAlamat");
+    const telp = document.getElementById("storeTelp");
+    const map = document.getElementById("mapFrame");
+
+    if (id === 1) {
+        title.innerText = "Toko Pandanaran";
+        alamat.innerText = "Jl. Pandanaran No.51A, Semarang";
+        telp.innerText = "Telp: +62 24 831 8531";
+        map.src = "https://www.google.com/maps?q=Dyriana+Bakery+Semarang&output=embed";
+    }
+
+    if (id === 2) {
+        title.innerText = "Toko Pamularsih";
+        alamat.innerText = "Jl. Pamularsih No.70, Semarang";
+        telp.innerText = "Telp: +62 24 766 35138";
+        map.src = "https://www.google.com/maps?q=Pamularsih+Semarang&output=embed";
+    }
+
+    if (id === 3) {
+        title.innerText = "Toko Ngaliyan";
+        alamat.innerText = "Jl. Prof Dr Hamka, Semarang";
+        telp.innerText = "Telp: +62 24 764 35005";
+        map.src = "https://www.google.com/maps?q=Ngaliyan+Semarang&output=embed";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const searchInput = document.getElementById("searchInput");
+
+    if (!searchInput) return; // biar ga error kalau ga ada input
+
+    searchInput.addEventListener("keyup", function () {
+
+        let keyword = this.value.toLowerCase();
+        let cards = document.querySelectorAll(".card");
+
+        cards.forEach(function (card) {
+
+            let title = card.querySelector("h4").innerText.toLowerCase();
+
+            if (title.includes(keyword)) {
+                card.style.display = "block";
+            } else {
+                card.style.display = "none";
+            }
+
+        });
+
+    });
+
+});
+
+let nama = card.querySelector("h4").innerText.toLowerCase();
+let title = card.querySelector("h4").innerText.toLowerCase();
